a525cfeb8db54830a5e134b330cc86e42e1f310f
Christopher Luna <mycql04@gmail.com>
2020-03-01 18:21:30 +0800

    Minify all packages that are bundled as UMD

--- packages/svg/rollup.config.js
+++ packages/svg/rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
+import { terser } from "rollup-plugin-terser";
 import pkg from './package.json';
 
 export default [
@@ -12,7 +13,10 @@ export default [
 				file: pkg.browser,
 				format: 'umd',
 				extend: true,
-				sourcemap: true
+        sourcemap: true,
+        plugins: [
+          (process.env.BUILD === 'production' && terser()),
+        ]
 			},
 			{
 				file: pkg.main,
